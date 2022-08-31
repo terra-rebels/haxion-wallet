0a5aaa8f791bc0c729f393c74a83af0f15653fe4
Frank <95719041+Ragepie@users.noreply.github.com>
2022-08-31 03:10:38 +0200

    Added solana support using third party (#1)

---
+++ Haxion.Solana/SolanaWalletGenerator.cs
@@ -0,0 +1,14 @@
+﻿using Solnet.Wallet;
+using Solnet.Wallet.Bip39;
+
+namespace Haxion.Solana
+{
+    public class SolanaWalletGenerator
+    {
+        public Wallet GenerateWallet ()
+        {
+            var mnemonic = new Mnemonic(WordList.English, WordCount.TwentyFour);
+            return new Wallet(mnemonic);
+        }
+    }
+}
--- HaxionWalletExec/Program.cs
+++ HaxionWalletExec/Program.cs
@@ -1,17 +1,10 @@
-﻿using Haxion.Core.Interfaces;
-using Haxion.ETH.Models;
-using NBitcoin;
-
-namespace HaxionWalletExec
+﻿namespace HaxionWalletExec
 {
     class TerraWalletGenerationExecutable
     {
-        // TODO NULS, NerveNetwork, Near, Chilliz, ZRX, Solana, Moonbeam, ThorChain
+        // TODO NULS, NerveNetwork, Near, Chilliz, ZRX, Moonbeam, ThorChain
         public static void Main(string[] args)
         {
-            Mnemonic mnemonic = new Mnemonic(Wordlist.English, WordCount.TwentyFour);
-            IHaxionWallet<EthereumWallet> ethWallet = new EthereumHaxionWallet(mnemonic.ToString(), "");
-            var address = ethWallet.GetAccount(0).GetExternalWallet(0);
         }
     }
 }
